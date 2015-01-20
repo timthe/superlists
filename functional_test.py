d8fba84621a765c1f5dd55014123243b9bf95c97
timthe <timthe@gmail.com>
2015-01-20 20:13:51 +0900

    p59 commit

--- functional_test.py
+++ functional_test.py
@@ -27,13 +27,20 @@ class NewVisitorTest(unittest.TestCase):
 	# She types 'Buy peacock feathers' into a text box
         inputbox.send_keys('Buy peacock feathers')
 
+
 	# When she hits enter, the page updates, and now page lists
 	# "1: Buy peacock feathers" as an item in a to-do lists table
-        inputbox.send_keys(Keys.Enter)
+        inputbox.send_keys(Keys.ENTER)
 
+        inputbox = self.browser.find_element_by_id('id_list_table')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
+	# self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table 00 its text was:\n%s" % (table.text,))
 	
         # There is still a text box inviting her to add another
         self.fail('Finish the test!')
