9d420439f4cfba367231f7f0d426dea2846335f5
timthe <timthe@gmail.com>
2015-01-20 20:21:50 +0900

    p60 commit

--- functional_test.py
+++ functional_test.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrive_it_later(self):
         # She goes to check out its homepage
         self.browser.get('http://localhost:8000')
@@ -31,16 +36,14 @@ class NewVisitorTest(unittest.TestCase):
 	# When she hits enter, the page updates, and now page lists
 	# "1: Buy peacock feathers" as an item in a to-do lists table
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_list_table')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
-	# self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table 00 its text was:\n%s" % (table.text,))
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 	
         # There is still a text box inviting her to add another
         self.fail('Finish the test!')
