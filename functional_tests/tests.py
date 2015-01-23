ac01cc3c04a1262b56dfb4df8ce8ef96144f569f
timthe <timthe@gmail.com>
2015-01-23 21:43:27 +0900

    p135 commit

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -10,8 +10,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
             if 'liveserver' in arg:
                 cls.server_url = 'http://' + arg.split('=')[1]
                 return
-            super().setUpClass()
-            cls.server_url = cls.live_server_url
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
 
     @classmethod
     def tearDownClass(cls):
