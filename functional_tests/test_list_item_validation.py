35676160f7eb0af770719c9c03c8913275c750fe
timthe <timthe@gmail.com>
2015-01-28 21:18:27 +0900

    Adjust new list view to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -16,7 +16,7 @@ class ItemValidationTest(FunctionalTest):
 
         # She tries again with some text for the item, which now works
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
-        self.check_for_row_int_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('1: Buy milk')
 
         # Perversely, she now decides to submit a second blank list item
         self.browser.find_element_by_id('id_new_item').send_keys('\n')
