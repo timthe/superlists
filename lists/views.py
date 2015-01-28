6089aa235493a22c166477426347854ee00cfe81
timthe <timthe@gmail.com>
2015-01-28 23:00:22 +0900

    Refactor list view to handle new item POSTs

--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
 
-# Create your views here.
+
 def home_page(request):
     return render(request, 'home.html')
 
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
     list_ = List.objects.create()
@@ -23,8 +19,12 @@ def new_list(request):
         return render(request, 'home.html', {"error": error}) 
     return redirect('/lists/%d/' %(list_.id))
 
-def add_item(request, list_id):
+
+def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
-    
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' %(list_.id,))
+    return render(request, 'list.html', {'list': list_})
+
+   
--- superlists/urls.py
+++ superlists/urls.py
@@ -5,7 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/', include('lists.urls')),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
