59d668635e8653f67002cb61ced75eeea7acabd6
timthe <timthe@gmail.com>
2015-01-31 00:48:50 +0900

    20150131 p212 commit

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -34,3 +34,16 @@ class ItemValidationTest(FunctionalTest):
 
         #self.fail('write me!')
 
+    def test_cannot_add_duplicate_items(self):
+        # Edith goes to the home page and start a new list
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Buy wellies\n')
+        self.check_for_row_in_list_table('1: Buy wellies')
+
+        # She accidentally tries to enter a duplicate item
+        self.get_item_input_box().send_keys('Buy wellies')
+
+        # She sees a helpful error message
+        self.check_for_row_in_list_table('1: Buy wellies')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You've already got this in your list")
