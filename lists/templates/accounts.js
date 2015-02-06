d10cdc66090825bc46dfd11083dab7435e25c53e
timthe <timthe@gmail.com>
2015-02-06 23:52:28 +0900

    20150206

---
+++ lists/templates/accounts.js
@@ -0,0 +1,13 @@
+/*global $  */
+
+var initialize = function(navigator) {
+	$('#id_login').on('click', function() {
+		navigator.id.request();
+	});
+};
+
+window.Superlists = {
+	Accounts: {
+		initialize: initialize
+	}
+};
